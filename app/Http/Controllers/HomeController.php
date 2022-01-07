6154cff0290248118fe5eb958c53adf2ec40341a
Rizky Okta Vandi Prawira <rizkyoktavandip@gmail.com>
2022-01-07 15:07:47 +0700

    commit pertama

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $data['judul'] = "Data Semua User";
+        $data['pengguna'] = User::all();
+
+        return view('home',$data);
+    }
+}
